bf651ef00ac8633dba05f40ac2501539a429dd66
JonClayton <jon@arundo.com>
2020-02-17 15:40:05 -0600

    Initial commit

---
+++ src/JonClayton.Packages/HelloWorld/Communication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JonClayton.Packages.HelloWorld
+{
+    public class Communication
+    {
+        public static string Greet(string name)
+        {
+            return "Greetings " + name;
+        }
+    }
+}
